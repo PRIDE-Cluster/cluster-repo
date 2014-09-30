9e9e0d94a54755fdde2dbf1cfdf15369bafdcd7e
harry.r.wang@gmail.com <harry.r.wang@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2014-09-30 15:38:41 +0000

    Removed transaction management from ClusterReader

--- spectra-cluster-repo/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/repo/dao/IClusterReadDao.java
+++ spectra-cluster-repo/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/repo/dao/IClusterReadDao.java
@@ -1,7 +1,12 @@
 package uk.ac.ebi.pride.spectracluster.repo.dao;
 
 
+import uk.ac.ebi.pride.spectracluster.repo.model.AssaySummary;
 import uk.ac.ebi.pride.spectracluster.repo.model.ClusterSummary;
+import uk.ac.ebi.pride.spectracluster.repo.model.PSMSummary;
+import uk.ac.ebi.pride.spectracluster.repo.model.SpectrumSummary;
+
+import java.util.List;
 
 /**
  * Dao interface for reading clusters from data source
@@ -25,5 +30,31 @@ public interface IClusterReadDao {
      * @return cluster
      */
     ClusterSummary findCluster(Long clusterId);
+
+    /**
+     * Find spectra using a list of spectrum references
+     * spectrum references are the id from the original database source
+     *
+     * @param spectrumReferences a list of spectrum references
+     * @return
+     */
+    List<SpectrumSummary> findSpectra(final List<String> spectrumReferences);
+
+
+    /**
+     * Find PSMs using a list of spectrum id
+     *
+     * @param spectrumIds a list of internal spectrum ids
+     * @return a list of PSMs
+     */
+    List<PSMSummary> findPSMBySpectrumId(final List<Long> spectrumIds);
+
+    /**
+     * Find assays using assay ids
+     *
+     * @param assayIds a list of internal assay ids
+     * @return a list of assays
+     */
+    List<AssaySummary> findAssays(final List<Long> assayIds);
 
 }
