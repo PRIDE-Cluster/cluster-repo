135ab3f70b7a67b8b2656fed019a007f9c757ba1
Rui Wang <harry.r.wang@gmail.com>
2015-05-11 15:15:29 +0100

    change both the species and the modification chats to have limited amount of items, and group the others into "Others" category

---
+++ src/test/java/uk/ac/ebi/pride/spectracluster/repo/FloatTest.java
@@ -0,0 +1,15 @@
+package uk.ac.ebi.pride.spectracluster.repo;
+
+import org.junit.Test;
+
+/**
+ * @author Rui Wang
+ * @version $Id$
+ */
+public class FloatTest {
+    @Test
+    public void testFloatClaculation() throws Exception {
+        float ratio = 101 * 1.0f / 104;
+        System.out.println(ratio);
+    }
+}
