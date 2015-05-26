0023027d6637a905e7d45b558fdbf3988a146c63
Rui Wang <harry.r.wang@gmail.com>
2015-05-26 14:57:39 +0100

    add additional web service end points for getting statistics

--- src/main/java/uk/ac/ebi/pride/spectracluster/repo/model/ClusterRepoStatistics.java
+++ src/main/java/uk/ac/ebi/pride/spectracluster/repo/model/ClusterRepoStatistics.java
@@ -8,6 +8,19 @@ package uk.ac.ebi.pride.spectracluster.repo.model;
  */
 public class ClusterRepoStatistics {
 
+    public static final String NUMBER_OF_CLUSTERS = "Number of clusters";
+    public static final String NUMBER_OF_HIGH_QUALITY_CLUSTERS = "Number of high quality clusters";
+    public static final String NUMBER_OF_MEDIUM_QUALITY_CLUSTERS = "Number of medium quality clusters";
+    public static final String NUMBER_OF_LOW_QUALITY_CLUSTERS = "Number of low quality clusters";
+    public static final String NUMBER_OF_SPECIES = "Number of species";
+    public static final String NUMBER_OF_PROJECTS = "Number of projects";
+    public static final String NUMBER_OF_ASSAYS = "Number of assays";
+    public static final String NUMBER_OF_DISTINCT_PEPTIDES = "Number of distinct peptides";
+    public static final String NUMBER_OF_IDENTIFIED_SPECTRA = "Number of identified spectra";
+    public static final String NUMBER_OF_CLUSTERS_PER_SPECIES = "Number of clusters per species";
+    public static final String NUMBER_OF_UNIQUE_PEPTIDES_PER_SPECIES = "Number of unique peptides per species";
+    public static final String OVERLAPPING_UNIQUE_PEPTIDES_ON_SPEICES = "Overlapping unique peptides on species";
+
     private String name;
     private Long value;
 
